4c3b897ca090f18435081afb12d11de4c9940cc9
jack6177733651 <jackshi3@gmail.com>
2024-01-24 02:03:27 -0500

    another commit

--- src/main/java/com/example/demo/Demo12Application.java
+++ src/main/java/com/example/demo/Demo12Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo12Application {
 
     public static void main(String[] args) {
+        System.out.println("hello world");
         SpringApplication.run(Demo12Application.class, args);
     }
 
